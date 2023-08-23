d02246763d58e19bea0095fd70fe4242046551b1
toavina <toavinathefirst@gmail.com>
2023-08-24 00:51:37 +0300

    isHired interpolation

--- src/app/userProfile.component.ts
+++ src/app/userProfile.component.ts
@@ -3,18 +3,36 @@ import { Component, Directive, ElementRef, Input } from "@angular/core";
 @Component({
   selector: "userProfile",
   template: `
-    <h3>firstName=</h3>
-    <h3>lastName=</h3>
-    <h2>job=</h2>
+    <h3>firstName= {{ first | uppercase }}</h3>
+    <h3>lastName= {{ last }}</h3>
+    <img [src]="source == '' ? '...' : source" alt="image du logo" />
+    <h2>job= {{ job }},({{ revenue | currency : "EUR" : "symbol" }})/mois</h2>
+    <p [class.hired]="isHired" ]></p>
   `,
+  styles: [
+    `
+      h3 {
+        color: brown;
+      }
+      .hired {
+        background: green;
+      }
+    `,
+  ],
 })
 export class UserProfileComponent {
-  @Input("firstName")
-  first = "Toavina";
-  @Input("lastName")
-  last = "ggg";
   @Input()
-  job = "";
+  revenue: number = 1200;
+
+  @Input("firstName")
+  first = "Andriam's";
+  @Input("lastName")
+  last = "Toavina";
+  @Input()
+  job = "developpeur";
+  source = "";
+  @Input()
+  isHired = false;
   constructor(private elementRef: ElementRef<HTMLElement>) {}
   ngOnInit() {}
 }
