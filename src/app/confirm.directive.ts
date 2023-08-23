9c5651d4ba16c74e2f5bc9beed47b840b37691af
toavina <toavinathefirst@gmail.com>
2023-08-23 22:29:08 +0300

    etes vous sur

--- src/app/confirm.directive.ts
+++ src/app/confirm.directive.ts
@@ -3,7 +3,7 @@ import { Directive, HostListener, Input } from "@angular/core";
 @Directive({ selector: "a[confirm]" })
 export class ConfirmDirective {
   @Input("confirmMessage")
-  message = "";
+  message = "ête vous sur?";
   @HostListener("click")
   clickConfirm() {
     return confirm(this.message);
